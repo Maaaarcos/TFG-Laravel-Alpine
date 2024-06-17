481314cf34177b2728285f1c96371cc56ee33b75
raul <1687975@alu.murciaeduca.es>
2024-06-18 00:20:02 +0200

    Cambios estilos general

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use App\Livewire\GestionInvetario;
 Route::middleware('guest')->group(function () {
     Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
-    // Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
-    // Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+    //Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+    //Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
 });
 
 // Rutas que requieren autenticación
